2696d0d748226d582813fbe3f11ed407e544f6f7
jhaprahlad <pkjha01234@gmail.com>
2023-06-08 11:00:31 +0530

    completed the basic structure of project

---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+// const urlModel = require('../model/urlModel') // importing urlmodel
+const { createUrl, getUrl } = require('../controller/urlContoller')
+
+
+// creating get and Post api 
+
+router.post("/url/shorten", createUrl);
+router.get("/:urlCode", getUrl)
+
+//exporting 
+module.exports = router;
+
